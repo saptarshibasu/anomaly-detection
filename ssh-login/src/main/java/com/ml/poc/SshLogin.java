557fec47cd4eff9d01b1a06ba8e74d5c330a115c
Saptarshi Basu <sapb2004@gmail.com>
2021-06-28 20:01:13 +0530

    data added

--- ssh-login/src/main/java/com/ml/poc/SshLogin.java
+++ ssh-login/src/main/java/com/ml/poc/SshLogin.java
@@ -1,6 +1,7 @@
 package com.ml.poc;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Random;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -14,12 +15,13 @@ public class SshLogin {
         String host = args[0];
         int port = 22;
         String command = "ls /";
+        Random random = new Random();
         while(true) {
-            listFolderStructure(username, password, host, port, command);
+            listFolderStructure(username, password, host, port, command, random);
         }
     }
 
-    public static String listFolderStructure(String username, String password, String host, int port, String command) throws Exception {
+    public static String listFolderStructure(String username, String password, String host, int port, String command, Random random) throws Exception {
         Session session = null;
         ChannelExec channel = null;
         String response = null;
@@ -36,7 +38,7 @@ public class SshLogin {
             channel.setErrStream(errorResponseStream);
             channel.connect();
             while (channel.isConnected()) {
-                Thread.sleep(100);
+                Thread.sleep(random.nextInt(1000));
             }
             String errorResponse = new String(errorResponseStream.toByteArray());
             response = new String(responseStream.toByteArray());
